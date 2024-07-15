8596cfeda89d797bcee85980356d3231f2fcac6a
Raj Deshmukh <anmol.soni@labourhomeindia.com>
2024-07-15 19:11:41 +0530

    pdf download through content disposition

--- controllers/productivity.js
+++ controllers/productivity.js
@@ -1,51 +1,93 @@
 //const User = require("../models/User");
 const excelJS = require("exceljs");
+const fs = require('fs');
 //const db = require('../db/db');
-const {getProductivityData, getFormattedDataHelper, getCategoriesForBookingID} = require('../helpers/productivityHelper')
-const createPdfWithBarChart  = require("../helpers/generateProductivityPDF");
-const path = require('path');
+const {
+  getProductivityData,
+  getFormattedDataHelper,
+  getCategoriesForBookingID,
+} = require("../helpers/productivityHelper");
+const createPdfWithBarChart = require("../helpers/generateProductivityPDF");
+const path = require("path");
 
 const getProductivityByCategory = async (req, res) => {
-    console.log(req.body)
-    const { type, category_id= "", filterBy = "daily", bookingId, date, month } = req.body;
-    
-    try {
-        const {productivityDlrData, productivityDprData} = await getProductivityData(type, bookingId, category_id, month);
-        const formatData = await getFormattedDataHelper({productivityDlrData, productivityDprData}, filterBy, date);
-        res.json(formatData);
-    } catch (ex) {
-        console.log(ex)
-    }
+  console.log(req.body);
+  const {
+    type,
+    category_id = "",
+    filterBy = "daily",
+    bookingId,
+    date,
+    month,
+  } = req.body;
+
+  try {
+    const { productivityDlrData, productivityDprData } =
+      await getProductivityData(type, bookingId, category_id, month);
+    const formatData = await getFormattedDataHelper(
+      { productivityDlrData, productivityDprData },
+      filterBy,
+      date
+    );
+    res.json(formatData);
+  } catch (ex) {
+    console.log(ex);
+  }
 };
 
 const downloadPDF = async (req, res) => {
-    console.log(req.body)
-    const { type, category_id= "", filterBy = "daily", bookingId, date, month } = req.body;
-    
-    try {
-        const {productivityDlrData, productivityDprData} = await getProductivityData(type, bookingId, category_id, month);
-        const formatData = await getFormattedDataHelper({productivityDlrData, productivityDprData}, filterBy, date);
-        await createPdfWithBarChart(formatData, filterBy, bookingId);
-        const filePath = path.join(__dirname, '../bar-chart.pdf');
-        res.download(filePath);
-    } catch (ex) {
-        console.log(ex)
-    }
+  console.log(req.body);
+  const {
+    type,
+    category_id = "",
+    filterBy = "daily",
+    bookingId,
+    date,
+    month,
+  } = req.body;
+
+  try {
+    const { productivityDlrData, productivityDprData } =
+      await getProductivityData(type, bookingId, category_id, month);
+    const formatData = await getFormattedDataHelper(
+      { productivityDlrData, productivityDprData },
+      filterBy,
+      date
+    );
+    await createPdfWithBarChart(formatData, filterBy, bookingId);
+    const filePath = path.join(__dirname, "../bar-chart.pdf");
+    res.setHeader("Content-Disposition", 'attachment; filename="example.pdf"');
+    res.setHeader('Content-Type', 'application/pdf');
+
+    const fileStream = fs.createReadStream(filePath);
+    fileStream.pipe(res);
+
+    fileStream.on('end', () => {
+      res.end();
+    });
+
+    fileStream.on('error', (error) => {
+      console.log(error);
+      res.status(500).send('An error occurred while processing your request.');
+    });
+  } catch (ex) {
+    console.log(ex);
+  }
 };
 
 const getCategories = async (req, res) => {
-    console.log(req.params)
+  console.log(req.params);
 
-    const {bookingId} = req.params;
-    if(!bookingId) {
-        res.json({data: "bookingID is invalid"});
-    }
-    try {
-        const data = await getCategoriesForBookingID(bookingId);
-        res.json(data);
-    } catch (ex) {
-        console.log(ex)
-    }
+  const { bookingId } = req.params;
+  if (!bookingId) {
+    res.json({ data: "bookingID is invalid" });
+  }
+  try {
+    const data = await getCategoriesForBookingID(bookingId);
+    res.json(data);
+  } catch (ex) {
+    console.log(ex);
+  }
 };
 
-module.exports = {getProductivityByCategory, getCategories, downloadPDF};
+module.exports = { getProductivityByCategory, getCategories, downloadPDF };
