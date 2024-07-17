13240210558b1c656ecc3f90cad09a14683fcde0
Raj Deshmukh <anmol.soni@labourhomeindia.com>
2024-07-17 14:40:32 +0530

    productivity api bug fixes

--- helpers/productivityHelper.js
+++ helpers/productivityHelper.js
@@ -11,7 +11,7 @@ const getProductivityData = async (
   bookingId,
   category_id,
   month,
-  {persona = "contractor", contractorId}
+  {persona, contractorId}
 ) => {
   const con = await db.getConnection();
   try {
